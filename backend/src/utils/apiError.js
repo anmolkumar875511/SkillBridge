0818cc2717f8c1a5878e4220786cad38b654aa33
Anmol Kumar <anmolkumar875511@gmail.com>
2026-01-01 10:57:41 +0530

    apiError (stanadard error format) added

---
+++ backend/src/utils/apiError.js
@@ -0,0 +1,21 @@
+class apiError extends Error {
+  constructor(
+    statusCode,
+    message = "Internal Server Error",
+    errors = [],
+    stack = null
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errors = errors;
+    this.stack = stack;
+    this.success = false;
+    if(stack) {
+      this.stack = stack;
+    } else {
+        Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export default apiError;
