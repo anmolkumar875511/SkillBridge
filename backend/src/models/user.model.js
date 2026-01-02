10e981796f215f4ddafd82d2074e638b4dc2f9e1
Anmol Kumar <anmolkumar875511@gmail.com>
2026-01-02 08:53:38 +0530

    methods and token added to user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -44,6 +45,9 @@ const userSchema = new mongoose.Schema(
     resume: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Resume"
+    },
+    refreshToken: {
+      type: String
     }
   },
   { timestamps: true }
@@ -53,6 +57,44 @@ userSchema.pre("save", async function () {
     if (!this.isModified("password")) return;
     this.password = await bcrypt.hash(this.password, 11);
 });
+
+userSchema.methods.isPasswordMatch = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.toJSON = function () {
+    const user = this.toObject();
+    delete user.password;
+    delete user.refreshToken;
+    return user;
+};
+
+userSchema.methods.generateAccessToken = function () {
+    const token = jwt.sign(
+      { id: this._id, role: this.role },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+    return token;
+};
+
+userSchema.methods.generateRefreshToken = function () {
+    const token = jwt.sign(
+      { id: this._id },
+      process.env.JWT_REFRESH_SECRET,
+      { expiresIn: process.env.JWT_REFRESH_EXPIRES_IN }
+    );
+    return token;
+};
+
+userSchema.methods.generateResetPasswordToken = function () {
+  const token = jwt.sign(
+    { id: this._id },
+    process.env.JWT_RESET_PASSWORD_SECRET,
+    { expiresIn: process.env.JWT_RESET_PASSWORD_EXPIRES_IN }
+  );
+  return token;
+};
 
 export default mongoose.model("User", userSchema);
 
