d75fd1ff60046069549e538f1f88c2da9d1e873e
Anmol Kumar <anmolkumar875511@gmail.com>
2026-01-02 10:52:27 +0530

    add user routes for authentication and profile management

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { verifyToken, authorizeRoles } from "../middlewares/auth.middleware";
+import { getUserProfile,
+    updateUserProfile,
+    changeUserPassword,
+    registerUser,
+    loginUser,
+    refreshAccessToken,
+    logoutUser
+} from "../controllers/user.controller.js";
+
+const router = Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/refresh-token", refreshAccessToken);
+
+router.post("/logout", verifyToken, logoutUser);
+router.get("/profile", verifyToken, getUserProfile);
+router.put("/profile", verifyToken, updateUserProfile);
+router.put("/change-password", verifyToken, changeUserPassword);
+
+export default router;
