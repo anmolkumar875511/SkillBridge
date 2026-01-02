e350af10627684f5eecc297e8fffdeb6e3b4d97b
Anmol Kumar <anmolkumar875511@gmail.com>
2026-01-02 10:18:12 +0530

    use accessToken instead of refreshToken

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json({ limit: '32kb' }));
-
+app.use(express.urlencoded({ extended: true, limit: '32kb' }));
+app.use(cookieParser());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 
 
 export { app };
