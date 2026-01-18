cb79d13d0fbd3a5c67e8720f5dd134c144a7ec5f
Anmol Kumar <anmolkumar875511@gmail.com>
2026-01-18 12:30:35 +0530

    route added for toggle task

--- backend/src/routes/roadmap.routes.js
+++ backend/src/routes/roadmap.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/auth.middleware.js";
-import { createRoadmap}  from "../controllers/roadmap.controller.js";
+import { createRoadmap, toggleTaskStatus}  from "../controllers/roadmap.controller.js";
 
 const router = Router();
 
 router.get("/gerenate/:opportunityId", verifyToken, createRoadmap);
+router.put("/toggle/:roadmapId/:taskId", verifyToken, toggleTaskStatus);
 
 export default router;
