d31884f8586ba7096596a7683d255465c3b9ed38
Anmol Kumar <anmolkumar875511@gmail.com>
2026-01-18 12:28:32 +0530

    controller added for toggle task

--- backend/src/controllers/roadmap.controller.js
+++ backend/src/controllers/roadmap.controller.js
@@ -23,3 +23,50 @@ export const createRoadmap = asyncHandler(async (req, res) => {
         )
     );
 });
+
+
+export const toggleTaskStatus = asyncHandler(async (req, res) => {
+    const { roadmapId, taskId } = req.params;
+
+    const roadmap = await Roadmap.findOne({
+        _id: roadmapId,
+        user: req.user._id
+    });
+
+    if (!roadmap) {
+        throw new apiError(404, "Roadmap not found");
+    }
+
+    let taskFound = false;
+    let completedTasks = 0;
+    let totalTasks = 0;
+
+    roadmap.roadmap.forEach((week) => {
+        week.tasks.forEach((task) => {
+            totalTasks++;
+            if (task._id === taskId) {
+                taskFound = true;
+                task.isCompleted = !task.isCompleted;
+            }
+            if (task.isCompleted) completedTasks++;
+        });
+    });
+
+    if (!taskFound) {
+        throw new apiError(404, "Task not found in this roadmap");
+    }
+
+    roadmap.progress = totalTasks === 0 ? 0 : Math.round((completedTasks / totalTasks) * 100);
+
+    await roadmap.save();
+
+    return res
+    .status(200)
+    .json(
+        new apiResponse(
+            200,
+            "Task status updated successfully",
+            roadmap
+        )
+    );
+});
