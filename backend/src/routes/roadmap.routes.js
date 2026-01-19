932ba14df14e451edc0a452a2a27ef5dc9ff94f0
Anmol Kumar <anmolkumar875511@gmail.com>
2026-01-19 20:36:03 +0530

    routes added of opportunities and roadmaps

--- backend/src/routes/roadmap.routes.js
+++ backend/src/routes/roadmap.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/auth.middleware.js";
-import { createRoadmap, toggleTaskStatus}  from "../controllers/roadmap.controller.js";
+import { createRoadmap, getRoadmap, toggleTaskStatus}  from "../controllers/roadmap.controller.js";
 
 const router = Router();
 
 router.get("/gerenate/:opportunityId", verifyToken, createRoadmap);
+router.get("/", verifyToken, getRoadmap);
 router.put("/toggle/:roadmapId/:taskId", verifyToken, toggleTaskStatus);
 
 export default router;
