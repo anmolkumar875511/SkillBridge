936bc1d6fb1fda68d8227a623f9bf6c7af146d49
Anmol Kumar <anmolkumar875511@gmail.com>
2026-01-30 00:52:47 +0530

    some more fixesss...

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,8 +17,10 @@ app.use(express.urlencoded({ extended: true, limit: '32kb' }));
 app.use(cookieParser());
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL,
+        origin: process.env.FRONTEND_URL, 
         credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"]
     })
 );
 
