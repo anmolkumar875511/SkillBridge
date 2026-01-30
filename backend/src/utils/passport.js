0a94a8efbfdb1aaa509e2923e8c9472df617ed7e
Anmol Kumar <anmolkumar875511@gmail.com>
2026-01-31 00:52:14 +0530

    passport updated

--- backend/src/utils/passport.js
+++ backend/src/utils/passport.js
@@ -9,7 +9,7 @@ passport.use(
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL:
                 process.env.NODE_ENV === 'production'
-                    ? 'https://skill-bridge-seven-gamma.vercel.app/api/v1/user/auth/google/callback'
+                    ? 'https://skillbridge-server-zeta.vercel.app/api/v1/user/auth/google/callback'
                     : 'http://localhost:5000/api/v1/user/auth/google/callback',
         },
         async (accessToken, refreshToken, profile, done) => {
