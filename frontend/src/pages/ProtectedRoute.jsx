996e50fac740fb839138a77fc2a235e0de63c345
Anmol Kumar <anmolkumar875511@gmail.com>
2026-01-31 09:38:37 +0530

    return Outlet is fixed

--- frontend/src/pages/ProtectedRoute.jsx
+++ frontend/src/pages/ProtectedRoute.jsx
@@ -1,26 +1,19 @@
-import { Navigate } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = () => {
   const { user, loading } = useContext(AuthContext);
 
-  console.log('ProtectedRoute Loading:', loading);
-  console.log('ProtectedRoute User:', user);
-
-  // ⏳ Still checking auth
   if (loading) {
-    return null; // or <AuthLoader />
+    return null;
   }
 
-  // ❌ Not logged in → NO API CALLS
   if (!user) {
     return <Navigate to="/login" replace />;
   }
 
-  // ✅ Logged in
-  return children;
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
-
