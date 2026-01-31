240d5c3fbb86db1c7eab00ea9c4cf0341670f9a8
Anmol Kumar <anmolkumar875511@gmail.com>
2026-01-31 14:51:07 +0530

    db URI fixed

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from '../utils/constants.js';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}?appName=Cluster0`);
         console.log(`\nMongoDB connected!!`);
     } catch (error) {
         console.error('MongoDB connection error:', error);
